ae5b73b3b77eae55e1b6113ef48657a641d74383
jasonyangftd <jasonyangftd@gmail.com>
2014-12-30 12:16:55 -0800

    Finish all basic requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,14 +4,13 @@ var app = {
   'text': 'trololo',
   'roomname': 'hr6',
   'server': 'https://api.parse.com/1/classes/chatterbox',
-
+  'friends': {}
 };
 
 var message = {};
 
 app.init = function(){
-    // console.log('a');
-    // app.fetch();
+
 };
 
 app.send = function(message){
@@ -42,41 +41,70 @@ app.fetch = function(){
     success: function (data) {
       console.log('chatterbox: Message retrieved');
       var results = data.results;
-      console.log(results);
       _.each(results, function(obj){
         app.addMessage(obj);
-        // $('.msg').append('<li>' + obj.username + ':' + obj.text + '</li>');
+        // app.addFriend(obj);
+
+      // console.log($("#" + obj.username));
       });
+
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to retrieve message');
     }
   });
-}
+  // event.preventDefault();
 
-// app.fetch();
+}
 
 app.addMessage = function(message){
   var $chat = $('<li></li>');
-  $chat.text(message.username + ": " + message.text);
+  var $user = $('<button id='+message.username+'>' + message.username + '</button>');
+  $chat.text(" " + message.text + ": " + message.roomname );
   // console.log('adding message', message);
-  $('#chats').prepend($chat);
+  $($chat).prepend($user);
+  $('#chats').append($chat);
+  $user.on('click', function(){
+    if( !app.friends[message.username] ){
+      app.friends[message.username] = true;
+      $('#friend').append('<li>' + message.username + '   is your friend!' + '</li>');
+    }
+  });
+};
+
+app.addFriend = function(){
+    if( !app.friends[message.username] ){
+      app.friends[message.username] = true;
+      $('#friend').append('<li>' + message.username + '</li>');
+    }
 };
 
 app.clearMessages = function(message){
   $('#chats').empty();
 };
 
+app.addRoom = function(name){
+  var $room = $('<option>' + name + '</option>');
+  $('#roomSelect').append($room);
+};
+
 
 $(document).ready(function(){
   $('.button').on('click', function(){
+    app.clearMessages();
     app.fetch();
+    // $("#" + message.username).on('click', function(){
+    //     console.log( "a" );
+    // });
   });
   $('.submit').on('click', function(){
     message['username'] = $("#username")[0].value;
     message['text'] = $("#message")[0].value;
+    message['roomname'] = $("#room")[0].value;
     app.send(message);
+    // event.preventDefault();
   })
 });
+
 
